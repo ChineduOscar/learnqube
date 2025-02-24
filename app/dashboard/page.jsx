3b5ce1ab687b667929a40d2c3d9b3f8c1e38afdb
ChineduOscar <chinedunnadoziepatrick@gmail.com>
2025-02-24 08:33:06 +0100

    fixing dashboard error

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,28 +1,36 @@
 'use client'
 import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 import Cookies from "js-cookie";
 
 const Dashboard = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
   const [token, setToken] = useState(null);
 
   useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const urlToken = urlParams.get("token");
+    const urlToken = searchParams.get("token");
 
     if (urlToken) {
+      // Save token in cookies
       document.cookie = `token=${urlToken}; path=/; secure; samesite=None;`;
+      setToken(urlToken);
 
       setTimeout(() => {
-        window.history.replaceState({}, "", window.location.pathname);
-        setToken(urlToken);
+        // Remove token from the URL (use relative path)
+        const params = new URLSearchParams(searchParams);
+        params.delete("token");
+
+        router.replace(`/dashboard${params.toString() ? `?${params.toString()}` : ""}`, { scroll: false });
       }, 1000);
     } else {
+      // Read token from cookies
       const storedToken = Cookies.get("token");
       if (storedToken) {
         setToken(storedToken);
       }
     }
-  }, []);
+  }, [searchParams, router]);
 
   return (
     <div>
