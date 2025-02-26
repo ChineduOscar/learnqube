49527b2965fcd4e5d543f6fc63640c4de32dd087
ChineduOscar <chinedunnadoziepatrick@gmail.com>
2025-02-26 06:14:43 +0100

    fixed the middleware error

--- middleware.js
+++ middleware.js
@@ -2,13 +2,19 @@ import { NextResponse } from "next/server";
 
 export function middleware(request) {
   const token = request.cookies.get("token")?.value;
-
+  const url = new URL(request.url);
+  
+  if (url.searchParams.has("token") && request.nextUrl.pathname.startsWith("/dashboard")) {
+    return NextResponse.next();
+  }
+  
   if (!token && request.nextUrl.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
-
+  
   return NextResponse.next();
 }
+
 export const config = {
   matcher: "/dashboard/:path*",
 };
